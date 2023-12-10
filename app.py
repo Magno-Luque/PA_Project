f5cbf3c14056a249abc1a44cd1418868760a5a4f
Magno-Luque <140691274+Magno-Luque@users.noreply.github.com>
2023-12-10 18:21:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ from PIL import Image
 from analisis_nacional import visualizacion_a_nivel_nacional
 from analisis_departamental import load_department_boundaries, load_data, assign_departments, show_departments_count
 from analisis_magnitud_1 import mostrar_dashboard
-st.set_page_config(
-    page_title="Sismos en el Perú",
-    page_icon="volcano",
-    initial_sidebar_state="expanded",
-)
-        #background-image: url("https://raw.githubusercontent.com/gcdavidq/Project_PA/main/imagenes/image2.jpg");
+
 page_bg_img = f"""
     <style>
     [data-testid="stAppViewContainer"] > .main {{
@@ -21,10 +16,11 @@ page_bg_img = f"""
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
-image1 = Image.open('imagenes/image1.png')
+image1 = Image.open('image1.png')
+
 
 # Añadimos un panel de control
-tab1, tab2, tab3 = st.tabs([":blue[**INICIO**]", ":blue[_**ANÁLISIS A NIVEL NACIONAL**_]", ":blue[_**ANÁLISIS A NIVEL DEPARTAMENTAL**_]"])
+tab1, tab2, tab3 = st.tabs([":blue[**INICIO**] :derelict_house_building:", ":blue[_Análisis a nivel nacional_]", ":blue[_Análisis a nivel departamental_]"])
 
 with tab1:
     st.image(image1)
@@ -32,9 +28,6 @@ with tab1:
 # Análisis a nivel nacional
 with tab2:
     visualizacion_a_nivel_nacional("Catalogo1960_2022.csv")
-    mostrar_dashboard("Catalogo1960_2022.csv")
-
-
 
 
 # Análisis a nivel departamental
